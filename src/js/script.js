53645c1c14bcda469dba489728b8e782d6c5b0cf
OliverVermeulen <72196313+OliverVermeulen@users.noreply.github.com>
2022-05-08 18:04:10 +0200

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -21,7 +21,6 @@ const Component = {
       watchList: [],
     };
   },
-
   // filter functions
   computed: {
     comingSoonList() {
@@ -43,18 +42,18 @@ const Component = {
       return filterGenre("Horror", this.itemList);
     },
   },
-
   // signs user out
   methods: {
+    // signs user out
     logout() {
       localStorage.removeItem(SIGNED_IN_KEY);
       window.location.href = "../../index.html";
     },
-
+    // button that locates items to myList.js page
     myListBtn() {
       window.location.href = "../pages/watch-list-page.html";
     },
-
+    // my list local storage function
     addToWatchList(item) {
       if (!localStorage.getItem(MY_LIST_KEY)) {
         let watchListArray = [];
@@ -70,135 +69,134 @@ const Component = {
 
   // html template
   template: /* html */ `
-
 <header id="myHeader">
-  <a href="#" class="netflix-logo"><img src="/src/images/netflix.png" alt="Netflix logo"/></a>
+<a href="#" class="netflix-logo"><img src="/src/images/netflix.png" alt="Netflix logo"/></a>
 
-  <ul class="navigation-list">
-    <li>Home</li>
-    <li><a href="./my-list.html">My List</a></li>
-  </ul>
+<ul class="navigation-list">
+<li>Home</li>
+<li><a href="./my-list.html">My List</a></li>
+</ul>
 
-  <div class="search-and-dropdown">
-    <div class="search">
-      <input type="search" placeholder="Titles, people, genres" id="search"/>
-      <i class="fa fa-search"></i>
-    </div>
-    <div class="dropdown">
-      <button class="pfp-button"><img src="/src/images/pfp.png" alt="profile picture" class="pfp"></button>
-      <div class="dropdown-content">
-        <p>{{ username }}</p>
-        <p @click="logout">Log out</p>
-      </div>
-    </div>
-  </div>
+<div class="search-and-dropdown">
+<div class="search">
+<input type="search" placeholder="Titles, people, genres" id="search"/>
+<i class="fa fa-search"></i>
+</div>
+<div class="dropdown">
+<button class="pfp-button"><img src="/src/images/pfp.png" alt="profile picture" class="pfp"></button>
+<div class="dropdown-content">
+<p>{{ username }}</p>
+<p @click="logout">Log out</p>
+</div>
+</div>
+</div>
 </header>
 
 <main id="myMain">
-  <section class="banner" v-for="item in comingSoonList.slice(0, 1)">
-    <video class="bg" autoplay muted loop>
-      <source :src="item.preview" type="video/mp4">
-    </video>
-    <div class="content">
-      <img :src="item.poster" class="movieTitle" :alt="item.name" />
-      <p>{{ item.description }}</p>
-      <div class="buttons">
-        <button class="button play"><a :href="item.trailer" target="_blank"><i class="fa fa-play"></i> Play</a></button>
-        <button class="button info" id="myBtn"><i class="fa fa-plus"></i> More Info</button>
+<section class="banner" v-for="item in comingSoonList.slice(0, 1)">
+<video class="bg" autoplay muted loop>
+<source :src="item.preview" type="video/mp4">
+</video>
+<div class="content">
+<img :src="item.poster" class="movieTitle" :alt="item.name" />
+<p>{{ item.description }}</p>
+<div class="buttons">
+<button class="button play"><a :href="item.trailer" target="_blank"><i class="fa fa-play"></i> Play</a></button>
+<button class="button info" id="myBtn"><i class="fa fa-plus"></i> More Info</button>
 
-    <!-- Modal -->
-    <div id="myModal" class="modal">
-      <div class="modal-content">
-        <div class="modal-banner">
-          <span class="close">&times;</span>
-          <video class="banner-video" autoplay muted loop>
-            <source :src="item.preview" type="video/mp4">
-          </video>
-          <div class="modal-header-content">
-            <span><img :src="item.poster" class="modalMovieTitle" :alt="item.name" /></span>
-            <div class="modal-buttons">
-            <button class="button modal-play"><a :href="item.trailer" target="_blank"><i class="fa fa-play"></i> Play</a></button>
-            <button class="modal-add"><i class="fa fa-plus"></i></button>
-          </div>
-        </div>
-    </div>
-    <h3>About {{ item.name }}</h3>
-    <div class="modal-body">
-      <div class="modal-description">
-      <p>{{ item.description }}</p>
-    </div>
-    <div class="modal-meta">
-      <p>Release: {{ item.releaseDate }}</p>
-      <p>Runtime: {{ item.runtime }}</p>
-      <p>Rating: {{ item.rating }}</p>
-    </div>
-      </div>
-      </div>
-      </div>
-      </div>
-    </div>
-  </section>
+<!-- Modal -->
+<div id="myModal" class="modal">
+<div class="modal-content">
+<div class="modal-banner">
+<span class="close">&times;</span>
+<video class="banner-video" autoplay muted loop>
+<source :src="item.preview" type="video/mp4">
+</video>
+<div class="modal-header-content">
+<span><img :src="item.poster" class="modalMovieTitle" :alt="item.name" /></span>
+<div class="modal-buttons">
+<button class="button modal-play"><a :href="item.trailer" target="_blank"><i class="fa fa-play"></i> Play</a></button>
+<button class="modal-add"><i class="fa fa-plus"></i></button>
+</div>
+</div>
+</div>
+<h3>About {{ item.name }}</h3>
+<div class="modal-body">
+<div class="modal-description">
+<p>{{ item.description }}</p>
+</div>
+<div class="modal-meta">
+<p>Release: {{ item.releaseDate }}</p>
+<p>Runtime: {{ item.runtime }}</p>
+<p>Rating: {{ item.rating }}</p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
 
-  <section id="content-grid">
-    <div class="container-heading">Action</div>
-    <div class="container">
-      <div class="box hover-button-display" v-for="(item, index) in actionList.slice(0, 6)" :id="item.id" :class="item.name" :alt="item.name">
-        <img class="bg" :src="item.poster" />
-        <button type="button" class="hover-button" @click="addToWatchList(actionList[index])"><i class="fa fa-plus"></i></button>
-      </div>
-    </div>
+<section id="content-grid">
+<div class="container-heading">Action</div>
+<div class="container">
+<div class="box hover-button-display" v-for="(item, index) in actionList.slice(0, 6)" :id="item.id" :class="item.name" :alt="item.name">
+<img class="bg" :src="item.poster" />
+<button type="button" class="hover-button" @click="addToWatchList(actionList[index])"><i class="fa fa-plus"></i></button>
+</div>
+</div>
 
-    <div class="container-heading">Comedy</div>
-      <div class="container">
-        <div class="box hover-button-display" v-for="(item, index) in comedyList.slice(0, 6)" :id="item.id" :class="item.name" :alt="item.name">
-          <img class="bg" :src="item.poster" />
-          <button type="button" class="hover-button" @click="addToWatchList(comedyList[index])"><i class="fa fa-plus"></i></button>
-        </div>
-      </div>
+<div class="container-heading">Comedy</div>
+<div class="container">
+<div class="box hover-button-display" v-for="(item, index) in comedyList.slice(0, 6)" :id="item.id" :class="item.name" :alt="item.name">
+<img class="bg" :src="item.poster" />
+<button type="button" class="hover-button" @click="addToWatchList(comedyList[index])"><i class="fa fa-plus"></i></button>
+</div>
+</div>
 
-    <div class="container-heading">Romance</div>
-    <div class="container">
-      <div class="box hover-button-display" v-for="(item, index) in romanceList.slice(0, 6)" :id="item.id" :class="item.name" :alt="item.name">
-        <img class="bg" :src="item.poster" />
-        <button type="button" class="hover-button" @click="addToWatchList(romanceList[index])"><i class="fa fa-plus"></i></button>
-      </div>
-    </div>
+<div class="container-heading">Romance</div>
+<div class="container">
+<div class="box hover-button-display" v-for="(item, index) in romanceList.slice(0, 6)" :id="item.id" :class="item.name" :alt="item.name">
+<img class="bg" :src="item.poster" />
+<button type="button" class="hover-button" @click="addToWatchList(romanceList[index])"><i class="fa fa-plus"></i></button>
+</div>
+</div>
 
-    <div class="container-heading">Horror</div>
-    <div class="container">
-      <div class="box hover-button-display" v-for="(item, index) in horrorList.slice(0, 6)" :id="item.id" :class="item.name" :alt="item.name">
-        <img class="bg" :src="item.poster" />
-        <button type="button" class="hover-button" @click="addToWatchList(horrorList[index])"><i class="fa fa-plus"></i></button>
-      </div>
-    </div>
-  </section>
+<div class="container-heading">Horror</div>
+<div class="container">
+<div class="box hover-button-display" v-for="(item, index) in horrorList.slice(0, 6)" :id="item.id" :class="item.name" :alt="item.name">
+<img class="bg" :src="item.poster" />
+<button type="button" class="hover-button" @click="addToWatchList(horrorList[index])"><i class="fa fa-plus"></i></button>
+</div>
+</div>
+</section>
 </main>
 
 <footer id="myFooter">
-  <div class="social-links">
-    <a href="https://www.instagram.com/oliver.vermeulen/" class="social-link" target="_blank"><i class="fab fa-instagram icon" alt="Instagram"></i></a>
-    <a href="https://github.com/OliverVermeulen" class="social-link" target="_blank"><i class="fab fa-github icon" alt="GitHub"></i></a>
-    <a href="https://www.linkedin.com/in/oliver-vermeulen-311221222/" class="social-link" target="_blank"><i class="fab fa-linkedin-in icon" alt="Linkedin"></i></a>
-  </div>
+<div class="social-links">
+<a href="https://www.instagram.com/oliver.vermeulen/" class="social-link" target="_blank"><i class="fab fa-instagram icon" alt="Instagram"></i></a>
+<a href="https://github.com/OliverVermeulen" class="social-link" target="_blank"><i class="fab fa-github icon" alt="GitHub"></i></a>
+<a href="https://www.linkedin.com/in/oliver-vermeulen-311221222/" class="social-link" target="_blank"><i class="fab fa-linkedin-in icon" alt="Linkedin"></i></a>
+</div>
 
-  <ul class="footer-grid">
-    <li class="footer-grid-item">Audio and Subtitles</li>
-    <li class="footer-grid-item">Audio Description</li>
-    <li class="footer-grid-item">Help Centre</li>
-    <li class="footer-grid-item">Gift Cards</li>
+<ul class="footer-grid">
+<li class="footer-grid-item">Audio and Subtitles</li>
+<li class="footer-grid-item">Audio Description</li>
+<li class="footer-grid-item">Help Centre</li>
+<li class="footer-grid-item">Gift Cards</li>
 
-    <li class="footer-grid-item">Media Centre</li>
-    <li class="footer-grid-item">Investor Relations</li>
-    <li class="footer-grid-item">Jobs</li>
-    <li class="footer-grid-item">Terms of Use</li>
+<li class="footer-grid-item">Media Centre</li>
+<li class="footer-grid-item">Investor Relations</li>
+<li class="footer-grid-item">Jobs</li>
+<li class="footer-grid-item">Terms of Use</li>
 
-    <li class="footer-grid-item">Privacy</li>
-    <li class="footer-grid-item">Legal Notices</li>
-    <li class="footer-grid-item">Cookie Preferences</li>
-    <li class="footer-grid-item">Corporate Information</li>
-  </ul>
+<li class="footer-grid-item">Privacy</li>
+<li class="footer-grid-item">Legal Notices</li>
+<li class="footer-grid-item">Cookie Preferences</li>
+<li class="footer-grid-item">Corporate Information</li>
+</ul>
 
-  <p>© 2022-2022 Viswinkel, Ltd.</p>
+<p>© 2022-2022 Viswinkel, Ltd.</p>
 </footer>
   `,
   mounted() {
